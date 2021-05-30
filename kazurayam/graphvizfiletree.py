6874d1436bd1df40e4b94a0cd58de9191548c4f3
kazurayam <kazuaki.urayama@gmail.com>
2021-05-30 09:56:19 +0900

    developping kazurayam/graphvizfiletree.py and kazurayam/graphvizfiletree_test.py

--- kazurayam/graphvizfiletree.py
+++ kazurayam/graphvizfiletree.py
@@ -1,8 +1,8 @@
-from io import StringIO
 import os.path
 from pathlib import Path
 from filevisitor import FileVisitor, FileVisitResult, Files
 from graphviz import Digraph
+
 
 class GraphvizFileTreeVisitor(FileVisitor):
 
@@ -12,22 +12,24 @@ class GraphvizFileTreeVisitor(FileVisitor):
 
     def pre_visit_directory(self, directory: Path) -> FileVisitResult:
         relative_path = os.path.relpath(directory, self.start)
-        self.buffer.write("> {}/\n".format(relative_path))
+        #self.buffer.write("> {}/\n".format(relative_path))
+        self.g.node(relative_path, relative_path, shape="folder")
         return FileVisitResult.CONTINUE
 
     def post_visit_directory(self, directory: Path, io_error: IOError) -> FileVisitResult:
         relative_path = os.path.relpath(directory, self.start)
-        self.buffer.write("< {}/\n".format(relative_path))
+        #self.buffer.write("< {}/\n".format(relative_path))
         return FileVisitResult.CONTINUE
 
     def visit_file(self, file: Path) -> FileVisitResult:
         relative_path = os.path.relpath(file, self.start)
-        self.buffer.write("= {}\n".format(relative_path))
+        #self.buffer.write("= {}\n".format(relative_path))
+        self.g.node(relative_path, relative_path)
         return FileVisitResult.CONTINUE
 
     def visit_file_failed(self, file: Path, io_error: IOError) -> FileVisitResult:
         relative_path = os.path.relpath(file, self.start)
-        self.buffer.write("! {}\n".format(relative_path))
+        #self.buffer.write("! {}\n".format(relative_path))
         return FileVisitResult.CONTINUE
 
 
@@ -35,19 +37,14 @@ class GraphvizMain:
 
     def __init__(self, starting_dir: Path):
         self.start = starting_dir
-        self.buffer = StringIO()
 
     def draw(self):
         g = Digraph("main", comment="File Tree graph")
         g.attr('graph', laout="dot", rank="max", rankdir="LR", splines="ortho")
-        g.node_attr.update(fontname="arial", fontsize="10")
+        g.node_attr.update(shape="rectangle", height="0.3", fontname="arial", fontsize="10")
         g.edge_attr.update(constraint="true", arrowhead="onormal")
-        ##
-        g.edge("A", "B")
-        #visitor = GraphvizFileTreeVisitor(self.start, self.buffer)
-        #Files.walk_file_tree(self.start, visitor)
-        #result = self.buffer.getvalue()
-        #self.buffer.close()
-        ##
-        return g
 
+        visitor = GraphvizFileTreeVisitor(self.start, g)
+        Files.walk_file_tree(self.start, visitor)
+
+        return g
